92f8171d51a5526098bf4f608cf127661e8afa6f
nicole-pimenta <nicole.pimenta@zoop.com.br>
2024-08-06 16:22:56 -0300

    feat:finalizando atividade1

---
+++ atividades/atividade1.js
@@ -0,0 +1,33 @@
+/*Precisamos imprimir um número para cada andar de um hotel de 20 andares. Porém, o dono do hotel é supersticioso e optou por não ter um 13ro andar.
+Escreva um código que imprima todos os números exceto o número 13.
+Escreva mais dois códigos que resolvam o mesmo problema, mas dessa vez usando os outros dois tipos de laços de repetição aprendidos.
+
+Como desafio, imprima eles em ordem decrescente (20, 19, 18...)*/
+
+for (let i = 1; i <= 20; i++) {
+  if (i !== 13) {
+    console.log(`Esse é o andar de número ${i}`);
+  }
+}
+
+//------------------------------------
+
+let j = 1;
+
+while (j <= 20) {
+  if (j !== 13) {
+    console.log(`Esse é o andar de número ${j}`);
+  }
+  j++;
+}
+
+//------------------------------------
+
+let k = 1;
+
+do {
+  if (k !== 13) {
+    console.log(`Esse é o andar de número ${k}`);
+  }
+  k++;
+} while (k <= 20);
